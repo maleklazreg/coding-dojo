e353ba24ca7cb2b7c7b463bfdc9ec834b5eb046a
malek lazreg <lazregmalek30@gmail.com>
2024-10-25 15:56:48 +0100

    Users with Bank Accounts

--- Python/Week1/Day2/Core/Users with Bank Accounts/python1.py
+++ Python/Week1/Day2/Core/Users with Bank Accounts/python1.py
@@ -1,22 +1,19 @@
-class BankAccount:
-    def __init__(self, name, email,):
+from python import BankAccount
+
+class User:
+    def __init__(self, name, email):
         self.name = name
         self.email = email
         self.account = BankAccount(int_rate=0.02, balance=0)
 
     def make_deposit(self, amount):
-        self.balence += amount
+        self.account.deposit(amount)
         return self
     
     def make_withdrawal(self, amount):
-        if self.display_balance >= amount:
-            self.balance -= amount
-        else:
-            print("Insufficient funds")
+        self.account.withdraw(amount)
         return self
-
     
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: {self.account.balance}")
         return self
-        
