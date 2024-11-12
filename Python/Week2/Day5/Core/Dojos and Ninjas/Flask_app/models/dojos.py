f403c224c011415926c53e3a6543953ca2bb3d0f
malek lazreg <lazregmalek30@gmail.com>
2024-11-13 00:13:24 +0100

    Dojos and Ninjas

--- Python/Week2/Day5/Core/Dojos and Ninjas/Flask_app/models/dojos.py
+++ Python/Week2/Day5/Core/Dojos and Ninjas/Flask_app/models/dojos.py
@@ -5,6 +5,8 @@ class Dojo:
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
 
 @classmethod
 def get_all(cls):
